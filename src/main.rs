8678c24bc1739f28b8c569c6c89b06cb85de9cee
Oliver Säfström <safstrom.oliver@gmail.com>
2023-05-02 15:25:55 +0200

    Refractor

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 #[macro_use]
 extern crate rocket;
 
-use std::ptr::copy_nonoverlapping;
 use rocket::futures::TryStreamExt;
 use rocket::http::Status;
 use rocket::response::status::Created;
 use rocket::serde::json::Json;
-use rocket_db_pools::{Connection, Database};
 use rocket_db_pools::mongodb::bson::doc;
 use rocket_db_pools::mongodb::bson::oid::ObjectId;
 use rocket_db_pools::mongodb::options::FindOptions;
+use rocket_db_pools::{Connection, Database};
 
 use crate::db_connection::MessagesDatabase;
 use crate::message::{Message, NewMessage, UserID};
@@ -23,21 +22,35 @@ fn health_check() -> Status {
 }
 
 #[get("/message?<to>&<fromMessageId>&<numberOfMessages>")]
-async fn get_message(db: Connection<MessagesDatabase>, user_id: UserID, to: &str, fromMessageId: &str, numberOfMessages: i64) -> Result<Json<Vec<Message>>, Status> { //-> Result<Json<Message>, Status> {
+async fn get_message(
+    db: Connection<MessagesDatabase>,
+    user_id: UserID,
+    to: &str,
+    fromMessageId: &str,
+    numberOfMessages: i64,
+) -> Result<Json<Vec<Message>>, Status> {
+    //-> Result<Json<Message>, Status> {
 
     let obj_id = ObjectId::parse_str(fromMessageId).expect("Error parsing ObjectID");
-    
+
     let filter = doc! { "_id": {"$gt": obj_id}, "from": user_id.as_ref(), "to": to };
 
     let options = FindOptions::builder().limit(numberOfMessages).build();
 
-    let mut cursor = db.database("postservice").collection::<Message>("messages")
-        .find(filter, options).await.expect("Failed to connect to database");
-
+    let mut cursor = db
+        .database("postservice")
+        .collection::<Message>("messages")
+        .find(filter, options)
+        .await
+        .expect("Failed to connect to database");
 
     let mut messages = vec![];
 
-    while let Some(message) = cursor.try_next().await.expect("Failed to get message from stream") {
+    while let Some(message) = cursor
+        .try_next()
+        .await
+        .expect("Failed to get message from stream")
+    {
         messages.push(message);
     }
     if messages.is_empty() {
@@ -45,7 +58,6 @@ async fn get_message(db: Connection<MessagesDatabase>, user_id: UserID, to: &str
     }
     Ok(Json(messages))
 }
-
 
 #[get("/message/<id>")]
 async fn get_by_id(db: Connection<MessagesDatabase>, id: &str) -> Result<Json<Message>, Status> {
@@ -56,9 +68,11 @@ async fn get_by_id(db: Connection<MessagesDatabase>, id: &str) -> Result<Json<Me
         Err(_) => return Err(Status::NoContent),
     };
 
-    let message = db.database("postservice").collection("messages")
-        .find_one(doc! {"_id": obj_id.unwrap()}
-                  , None).await;
+    let message = db
+        .database("postservice")
+        .collection("messages")
+        .find_one(doc! {"_id": obj_id.unwrap()}, None)
+        .await;
     match message {
         Ok(message) => Ok(Json(message.expect("Message not found"))),
         Err(_) => Err(Status::NoContent),
@@ -72,21 +86,26 @@ async fn new_message(
     user_id: UserID,
 ) -> Result<Created<Json<Message>>, Status> {
     let message = Message::new(new_message.into_inner(), user_id);
-    let added_message = db.database("postservice")
+    let added_message = db
+        .database("postservice")
         .collection("messages")
         .insert_one(message, None)
         .await
         .expect("Unable to insert message");
     //TODO to and message cannot be empty
 
-    Ok(Created::new(format!("/message/{}", added_message.inserted_id.as_object_id().unwrap())))
+    Ok(Created::new(format!(
+        "/message/{}",
+        added_message.inserted_id.as_object_id().unwrap()
+    )))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .attach(MessagesDatabase::init())
-        .mount("/", routes![health_check, new_message,get_by_id,get_message])
+    rocket::build().attach(MessagesDatabase::init()).mount(
+        "/",
+        routes![health_check, new_message, get_by_id, get_message],
+    )
 }
 
 #[cfg(test)]
