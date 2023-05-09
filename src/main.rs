9127e748f75a5db7b110ebc95690e5e230579d42
Oliver Säfström <safstrom.oliver@gmail.com>
2023-05-09 10:09:45 +0200

    Test connection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
+use std::future::Future;
+use lapin::{ConnectionProperties, Connection, Queue, types::FieldTable, BasicProperties, Channel};
+use lapin::options::*;
 use micro_post_service::db_connection::MessagesDatabase;
 use micro_post_service::endpoints::chat::get_chat_messages;
 use micro_post_service::endpoints::conversations::get_conversations;
@@ -11,7 +14,11 @@ use micro_post_service::models::cors;
 use rocket_db_pools::Database;
 
 #[launch]
-pub fn rocket() -> _ {
+pub async fn rocket() -> _ {
+    let rabbit = connect_rabbitMQ().await;
+    publish_message(rabbit).await;
+
+
     rocket::build()
         .attach(MessagesDatabase::init())
         .attach(cors::CORS)
@@ -25,4 +32,27 @@ pub fn rocket() -> _ {
                 get_conversations
             ],
         )
+}
+
+async fn publish_message(rabbit: Channel) {
+    rabbit.basic_publish("",
+                         "hello",
+                         BasicPublishOptions::default(),
+                         "hellorabbit".as_ref(),
+                         BasicProperties::default(), ).await.expect("Failed to publish").await.expect("Npe");
+}
+
+async fn connect_rabbitMQ() -> Channel {
+    // let addr = std::env::var("ROCKET_RABBIT_HOST").expect("Rocket adress not found.");
+
+
+    let rabbit = Connection::connect("amqp://localhost:5672", ConnectionProperties::default()).await.expect("Rabbitmq failed to connect");
+    println!("Rabbit connectec");
+    let channel = rabbit.create_channel().await.expect("Couldnot create queue");
+    channel.queue_declare(
+        "hello",
+        QueueDeclareOptions::default(),
+        FieldTable::default(), )
+        .await.expect("Could not create queue");
+    channel
 }
